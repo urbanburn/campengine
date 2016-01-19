2a31882ceedf205064981c74a456601812f843ca
Jesse Bastide <jbastide001@yahoo.com>
2016-01-19 20:22:09 +0100

    Add omniauth fields to users table; Add config options in config/initializers/devise.rb for facebook.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115120049) do
+ActiveRecord::Schema.define(version: 20160119190540) do
 
   create_table "camps", force: :cascade do |t|
     t.string   "name",                    null: false
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20160115120049) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
