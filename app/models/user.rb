40ce892c4ebc5f9bdaec6843d09bf2a146aff1ec
Jesse Bastide <jbastide001@yahoo.com>
2016-01-20 04:00:43 +0100

    Refactor devise line in user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # Can we put this on the same line as the other Devise opts?
-  # Test this.
-  devise :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :memberships
   has_many :camps, through: :memberships
