d762abc06f45b5abac323961c3031eb250b7f044
Jacob Burenstam <burenstam@gmail.com>
2016-01-21 20:46:16 +0100

    Add Camp#creator

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -1,4 +1,8 @@
 class Camp < ActiveRecord::Base
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+
   has_many :memberships
   has_many :users, through: :memberships
+
+  validates :creator, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119190540) do
+ActiveRecord::Schema.define(version: 20160121193451) do
 
   create_table "camps", force: :cascade do |t|
     t.string   "name",                    null: false
@@ -27,7 +27,10 @@ ActiveRecord::Schema.define(version: 20160119190540) do
     t.string   "contact_name",            null: false
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.integer  "user_id"
   end
+
+  add_index "camps", ["user_id"], name: "index_camps_on_user_id"
 
   create_table "memberships", force: :cascade do |t|
     t.datetime "created_at", null: false
