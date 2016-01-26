31edfec3064f38e091177183522b486eee0c8216
Jesse Bastide <jbastide001@yahoo.com>
2016-01-26 09:45:45 +0100

    Add helper method to user model for handling facebook callback.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ActiveRecord::Base
 
   has_many :memberships
   has_many :camps, through: :memberships
+
+
+  # Again, from Rails Girls tutorial on Facebook auth.
+  # Used for handling the facebook auth callback.
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      #user.name = auth.info.name # We don't persist usernames to the DB.
+    end
+  end
+
 end
