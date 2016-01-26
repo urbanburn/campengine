17b12afb72a828955f74fcec7aab7f91ff7ca10f
Jesse Bastide <jbastide001@yahoo.com>
2016-01-26 09:34:02 +0100

    Modify devise route to use omniauth controller for callbacks.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'camps#index'
-  devise_for :users
+  devise_for :users,
+    :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } 
   resources :camps do
     resources :images
   end
