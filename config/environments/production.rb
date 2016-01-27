c71b977d5768e6b9b0de92eb94975d391038406a
Fredrik Rubensson <fredrik@eldfluga.se>
2016-01-27 21:49:47 +0100

    more mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # needed for urls in mails
-  Rails.application.routes.default_url_options[:host] = 'services.urbanburn.se'
+  Rails.application.routes.default_url_options[:host] = 'creations.urbanburn.se'
 end
